de705ffe00c92b94bafcd90aedecc18a97e94d56
John P Clements <codeoloco@gmail.com>
2012-03-23 10:12:12 -0400

    through 8.2

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -31,14 +31,19 @@ describe "Authentication" do
             let(:user) { FactoryGirl.create(:user) }
             before do
                 fill_in "Email",    with: user.email
-                fill_in "Paasword", with: user.password
-                click_button sign_in
+                fill_in "Password", with: user.password
+                click_button "Sign in"
             end
 
             it { should have_selector('title', text: user.name) }
             it { should have_link('Profile', href: user_path(user)) }
             it { should have_link('Sign out', href: signout_path) }
             it { should_not have_link('Sign in',  href: signin_path) }
+
+            describe "followed by signout" do
+                before { click_link "Sign out" }
+                it { should have_link('Sign in') }
+            end
         end
     
     end
